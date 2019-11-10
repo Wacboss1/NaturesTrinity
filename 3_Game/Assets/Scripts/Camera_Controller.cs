09e2b02671b0a344de04b0a0729d0f630442ff3e
William Clemmons Jr <wacboss@live.com>
2019-11-09 18:22:09 -0600

    Created first area of map

---
+++ 3_Game/Assets/Scripts/Camera_Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour
+{
+    [SerializeField] float cameraHeight = -10;
+    Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, cameraHeight);
+    }
+}
--- 3_Game/Assets/Scripts/Player_Controller.cs
+++ 3_Game/Assets/Scripts/Player_Controller.cs
@@ -7,7 +7,8 @@ public class Player_Controller : MonoBehaviour
     public float speed = 1f;
     public float dashSpeed = 10;
 
-    [SerializeField] character charact;
+    [SerializeField] float dodgetime = 1;
+    [SerializeField] character charact; 
 
     Game_Controller controller;
     BoxCollider2D[] colliders;
@@ -52,11 +53,12 @@ public class Player_Controller : MonoBehaviour
         }
     }
 
+    //dashed the player and makes them immune to damage
     private void dodge()
     {
         speed += dashSpeed;
         controller.setHitable(false);
-        StartCoroutine(controller.recover(.5f));
+        StartCoroutine(controller.recover(dodgetime));
     }
 
     private void OnTriggerStay2D(Collider2D collision)
